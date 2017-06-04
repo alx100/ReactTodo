f39aa5e400f2749e48e535c2879e39f08f48f5e4
Alx100 <stotsky.alex@gmail.com>
2017-06-04 18:06:48 +0300

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -3,7 +3,7 @@ var $ = require('jquery');
 
 module.exports = {
     setTodos: function (todos) {
-        if($.isArray(todos)){
+        if ($.isArray(todos)) {
             localStorage.setItem('todos', JSON.stringify(todos));
             return todos;
         }
@@ -19,12 +19,40 @@ module.exports = {
         }
 
         return $.isArray(todos) ? todos : [];
-
         // or
         // if ($.isArray(todos)) {
         //     return todos;
         // } else {
         //     return [];
         // }
+    },
+    filterTodos: function (todos, showCompleted, searchText) {
+        var filteredTodos = todos;
+
+        // Filter by showCompleted
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        });
+
+        // Filter by searchText
+        filteredTodos = filteredTodos.filter((todo) => {
+            var text = todo.text.toLowerCase();
+            return searchText.length === 0 || text.indexOf(searchText) > -1;
+        });
+
+
+        // Sort todos with non-completed first
+        filteredTodos.sort((a, b) => {
+            if (!a.completed && b.completed) {
+                return -1;
+            } else if (a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        return filteredTodos;
     }
-}
+
+};
